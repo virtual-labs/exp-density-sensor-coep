6f7473210b9124e0674fd77c352edb4137ed0f13
vleadadmin <admin@vlabs.ac.in>
2023-12-05 05:42:28 +0000

    https://virtual-labs.github.io/virtual-labs/exp-density-sensor-coep click on the link to test your code.

---
+++ procedure.md
@@ -0,0 +1,8 @@
+### Procedure
+
+- Select service .
+- Select flow.
+- Click on simulation button.
+- Observe density on available display.
+- Calculate Frequency .
+- Repeat steps for another service.
---
+++ simulation/src/ServiceJson.js
@@ -0,0 +1,80 @@
+tempMasterJson = {};
+var masterJsonArr = [];
+
+masterJson = {};
+
+//masterJson = {
+//		"materialType": "sample1",
+//		"density": "1033",
+//		"frequency":"2600",	
+//};
+//masterJsonArr.push(masterJson);
+masterJson = {
+		"materialType": "Honey",
+		"density": "1400",
+		"frequency":"2550",	
+};
+
+masterJsonArr.push(masterJson);
+masterJson = {
+		"materialType": "Milk",
+		"density": "1033",
+		"frequency":"2780",	
+};
+
+masterJsonArr.push(masterJson);
+masterJson = {
+	"materialType": "Water",
+	"density": "1000",
+	"frequency":"2800",	
+};
+
+masterJsonArr.push(masterJson);
+
+//masterJson = {
+//		"materialType": "sample3",
+//		"density": "1200",
+//		"frequency":"2850",	
+//	};
+//
+//	masterJsonArr.push(masterJson);
+masterJson = {
+		"materialType": "Disel",
+		"density": "830",
+		"frequency":"2970",	
+};
+masterJsonArr.push(masterJson);
+
+masterJson = {
+		"materialType": "Petrol",
+		"density": "720",
+		"frequency":"3100",	
+};
+
+masterJsonArr.push(masterJson);
+//masterJson = {
+//		"materialType": "sample1",
+//		"density": "900",
+//		"frequency":"2900",	
+//};
+//
+//masterJsonArr.push(masterJson);
+//masterJson = {
+//		"materialType": "sample2",
+//		"density": "950",
+//		"frequency":"2850",	
+//};
+//
+//masterJsonArr.push(masterJson);
+
+
+console.log(masterJsonArr);
+
+
+
+
+
+
+
+
+
